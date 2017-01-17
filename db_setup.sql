29c14a2982689d5f8c8dfac7038cadbb3d07b8a2
Agam Agarwal <agammaster@gmail.com>
2017-01-17 22:06:13 +0530

    added user_id to name mapping table

--- db_setup.sql
+++ db_setup.sql
@@ -1,1 +1,10 @@
-create table subscriptions (ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL, OP_ID TEXT NOT NULL, SUBSCRIBER_ID TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS subscriptions (
+	id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	op_id TEXT NOT NULL,
+	subscriber_id TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_id_to_name (
+	user_id TEXT NOT NULL UNIQUE,
+	display_name TEXT NOT NULL
+);
