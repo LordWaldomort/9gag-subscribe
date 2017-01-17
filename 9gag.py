6858f6e81767e1126220b1690b5ba119c8e0d242
Agam Agarwal <agammaster@gmail.com>
2017-01-17 21:05:29 +0530

    fetch notifs in a loop

--- 9gag.py
+++ 9gag.py
@@ -7,10 +7,11 @@ CONFIG_FILE = 'config.cfg'
 
 BASE_URL = 'https://9gag.com'
 LOGIN = '/login'
-NOTIFICATION = '/notifications/load_more'
+NOTIFICATION = '/notifications/load_more?refKey='
 
 COMMENT_MENTION_REGEX = re.compile('<li [^>]* data-actionType="COMMENT_MENTION" [^>]*>')
 COMMENT_ID_REGEX = re.compile('.*data-objectId="http://9gag.com/gag/([^#]*)#([^"]*)".*')
+NOTIFICATION_NEXT_KEY_REGEX = re.compile('<li class=".*badge-notification-nextKey[^"]*">([^<]*)</li>')
 
 login_data = {'username': '***', 'password': '***'}
 last_notification_parsed = ''
@@ -26,16 +27,24 @@ def login(session):
 
 def get_new_notifications(session):
 	global last_notification_parsed
-	r = session.get(BASE_URL + NOTIFICATION)
-	notifs = COMMENT_MENTION_REGEX.findall(r.text)
 
 	comments = []
+	found_last = False
+	next_key = ''
 
-	for notif in notifs:
-		m = COMMENT_ID_REGEX.match(notif)
-		if m.group(2) == last_notification_parsed:
+	while not found_last:
+		r = session.get(BASE_URL + NOTIFICATION + next_key)
+		notifs = COMMENT_MENTION_REGEX.findall(r.text)
+		for notif in notifs:
+			m = COMMENT_ID_REGEX.match(notif)
+			if m.group(2) == last_notification_parsed:
+				found_last = True
+				break
+			comments.append((m.group(1), m.group(2)))
+		next_key_all = NOTIFICATION_NEXT_KEY_REGEX.findall(r.text)
+		if len(next_key_all) == 0 or len(next_key_all[0]) == 0:
 			break
-		comments.append((m.group(1), m.group(2)))
+		next_key = next_key_all[0]	# Assume first one to be the correct one
 
 	if len(comments) > 0:
 		last_notification_parsed = comments[0][1]
